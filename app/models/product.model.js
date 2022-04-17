05ee8dfce62155c955c260de1d4066665c65bce7
storres20 <sanchotorres777@gmail.com>
2022-04-17 05:03:09 -0500

    add product model

---
+++ app/models/product.model.js
@@ -0,0 +1,101 @@
+const sql = require("./db.js");
+
+// constructor
+const Product = function(product) {
+  this.name = product.name;
+  this.url_image = product.url_image;
+  this.price = product.price;
+  this.discount = product.discount;
+  this.category = product.category;
+};
+
+/* Product.create = (newProduct, result) => {
+  sql.query("INSERT INTO products SET ?", newProduct, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    console.log("created product: ", { id: res.insertId, ...newProduct });
+    result(null, { id: res.insertId, ...newProduct });
+  });
+}; */
+
+Product.findById = (id, result) => {
+  sql.query(`SELECT * FROM product WHERE id = ${id}`, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    if (res.length) {
+      console.log("found product: ", res[0]);
+      result(null, res[0]);
+      return;
+    }
+
+    // not found Product with the id
+    result({ kind: "not_found" }, null);
+  });
+};
+
+Product.findByCat = (cat, result) => {
+  sql.query(`SELECT * FROM product WHERE category = ${cat}`, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    if (res.length) {
+      console.log("found product: ", res);
+      result(null, res);
+      return;
+    }
+
+    // not found Product with the id
+    result({ kind: "not_found" }, null);
+  });
+};
+
+Product.findBySearch = (text, result) => {
+  sql.query(`SELECT * FROM product WHERE name LIKE '%${text}%'`, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    if (res.length) {
+      console.log("found product: ", res);
+      result(null, res);
+      return;
+    }
+
+    // not found Product with the TEXT
+    result({ kind: "not_found" }, null);
+  });
+};
+
+Product.getAll = (title, result) => {
+  let query = "SELECT * FROM product";
+
+  if (title) {
+    query += ` WHERE title LIKE '%${title}%'`;
+  }
+
+  sql.query(query, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    console.log("products: ", res);
+    result(null, res);
+  });
+};
+
+module.exports = Product;
