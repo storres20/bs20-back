8090e1fd91a5d381cea628f70e346887556b4589
storres20 <sanchotorres777@gmail.com>
2022-04-17 04:55:41 -0500

    connection to database

---
+++ app/config/db.config.js
@@ -0,0 +1,7 @@
+// Connection with Bsale database on aws
+module.exports = {
+  HOST: "mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com",
+  USER: "bsale_test",
+  PASSWORD: "bsale_test",
+  DB: "bsale_test"
+};
